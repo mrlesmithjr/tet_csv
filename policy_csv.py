bbcf652f5a597ea1e4ecc9468b6f730a02346d3b
Chris McHenry <mchenry.chris@gmail.com>
2017-07-14 15:41:36 -0400

    Updated to 2.0

--- policy_csv.py
+++ policy_csv.py
@@ -5,7 +5,61 @@ from apicservice import ConfigDB
 import json
 import argparse
 import csv
+from TetPolicy2 import Environment
 
+def v2tov1(config):
+
+    external_apps = []
+    external_app_ids = []
+    for invFilter in config['inventory_filters']:
+        external_apps.append(invFilter['name'])
+        external_app_ids.append(invFilter['id'])
+    #print external_app_ids
+
+    policies = []
+    for policy in config['default_policies']:
+        if ((policy['consumer_filter_id'] not in external_app_ids) and (policy['provider_filter_id'] not in external_app_ids) and policy['consumer_filter_id'] != policy['provider_filter_id']):
+            v1_policy = {}
+            v1_policy['src']=policy['consumer_filter_id']
+            v1_policy['dst']=policy['provider_filter_id']
+            v1_policy['src_name']=policy['consumer_filter_name']
+            v1_policy['dst_name']=policy['provider_filter_name']
+            v1_policy['whitelist']=[]
+            for whitelist in policy['l4_params']:
+                whitelist['action']='ALLOW'
+                v1_policy['whitelist'].append(whitelist)
+            policies.append(v1_policy)
+    config['policies']=policies
+
+    clusters = []
+    for cluster in config['clusters']:
+            v1_cluster = {}
+            v1_cluster['name']=cluster['name']
+            v1_cluster['id']=cluster['id']
+            v1_cluster['external']=cluster['external']
+            v1_cluster['nodes']=cluster['nodes']
+            v1_cluster['route_tag']={"route_tag": {"subnet_mask": "0.0.0.0/0","name": "N/A"}}
+            v1_cluster['labels']=[]
+            clusters.append(v1_cluster)
+
+    #print json.dumps(clusters)
+
+    config.pop('clusters',None)
+    config['clusters']=clusters
+    config['applications']=[]
+
+    config.pop('primary',None)
+    config.pop('absolute_policies',None)
+    config.pop('catch_all_action',None)
+    config.pop('app_scope_id',None)
+    config.pop('default_policies',None)
+    config.pop('app_scope_id',None)
+    config.pop('inventory_filters',None)
+    config.pop('created_at',None)
+    config.pop('author',None)
+    config.pop('description',None)
+
+    return config
 
 def main():
     """
@@ -49,40 +103,58 @@ def main():
         print 'Could not load improperly formatted protocols file'
         return
 
-    cdb = ConfigDB()
-    cdb.store_config(config)
-    clusters = cdb.get_epg_policies()
-    policies = cdb.get_contract_policies()
+    # Load in the IANA Ports
+    udp = {}
+    tcp = {}
+    ports = {}
+    try:
+        with open('service-names-port-numbers.csv') as port_file:
+            reader = csv.DictReader(port_file)
+            for row in reader:
+                if row['Transport Protocol'] == 'tcp':
+                    tcp[row['Port Number']] = row
+                elif row['Transport Protocol'] == 'udp':
+                    udp[row['Port Number']] = row
+    except IOError:
+        print '%% Could not load protocols file'
+        return
+    except ValueError:
+        print 'Could not load improperly formatted protocols file'
+        return
+    ports['UDP'] = udp
+    ports['TCP'] = tcp
+
+    #Select Tetration Apps and Load Tet Object Model
+    tetEnv = Environment()
+    tetEnv.loadPolicyFromFile(config=config)
+    app = tetEnv.primaryApps[tetEnv.primaryApps.keys()[0]]
+    clusters = app.clusters
+    filters = app.inventoryFilters
+    policies = app.defaultPolicies
+
 
     #Process nodes and output information to CSV
-    with open('nodes.csv', 'wb') as csvfile:
+    with open(app.name+'-hosts.csv', 'wb') as csvfile:
         nodeswriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        nodeswriter.writerow(['Name','IP','Cluster'])
-        for cluster in clusters:
-            ##Uncomment for ASA Config
-            #print "object-group network " + cluster.name.replace(' ','_')
-            for node in cluster.get_node_policies():
-                ##Uncomment for ASA Config
-                #print "network-object host " + node.ip
-                nodeswriter.writerow([node.name,node.ip,cluster.name])
+        nodeswriter.writerow(['Name','IP','Cluster','Application'])
+        for key in clusters.keys():
+            cluster = clusters[key]
+            for host in cluster.hosts:
+                nodeswriter.writerow([host['name'],host['ip'],cluster.name,app.name])
 
     #Process policies and output information to CSV
-    with open('policies.csv','wb') as csvfile:
+    with open(app.name+'-policies.csv','wb') as csvfile:
         policywriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        policywriter.writerow(['Source Cluster','Destination Cluster','Port Min','Port Max','Protocol'])
+        policywriter.writerow(['Source Cluster','Destination Cluster','Port Min','Port Max','Protocol','Application'])
         for policy in policies:
-            for rule in policy.get_whitelist_policies():
-                policywriter.writerow([policy.src_name,policy.dst_name,rule.port_min,rule.port_max,protocols[rule.proto]['Keyword']])
-                ##Uncomment for ASA Config
-                #if rule.proto == '1':
-                #    print "access-list ACL_IN extended permit " + protocols[rule.proto]['Keyword'] + " object-group " + policy.src_name.replace(' ','_') + " object-group " + policy.dst_name.replace(' ','_')
-                #elif (rule.proto == '6') or (rule.proto == '17'):
-                #    if rule.port_min == rule.port_max:
-                #        print "access-list ACL_IN extended permit " + protocols[rule.proto]['Keyword'] + " object-group " + policy.src_name.replace(' ','_') + " object-group " + policy.dst_name.replace(' ','_') + " eq " + rule.port_min
-                #    else:
-                #        print "access-list ACL_IN extended permit " + protocols[rule.proto]['Keyword'] + " object-group " + policy.src_name.replace(' ','_') + " object-group " + policy.dst_name.replace(' ','_') + " range " + rule.port_min + "-" + rule.port_max
+            for rule in policy.l4params:
+                if ((protocols[str(rule['proto'])]['Keyword'] == 'TCP') or (protocols[str(rule['proto'])]['Keyword'] == 'UDP')) and (rule['port_min'] in ports[protocols[str(rule['proto'])]['Keyword']].keys()):
+                    app = ports[protocols[str(rule['proto'])]['Keyword']][rule['port_min']]['Description']
+                else:
+                    app = ''
+                policywriter.writerow([policy.consumerFilterName,policy.providerFilterName,rule['port_min'],rule['port_max'],protocols[str(rule['proto'])]['Keyword'],app])
 
 
 if __name__ == '__main__':
